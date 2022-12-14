ab88fe7fa0328ba6ecbca36b2c289cd8ad10db16
Sheetu1807 <sdussooa@gmail.com>
2022-12-14 01:12:33 +0000

    1

--- src/Firstclass.java
+++ src/Firstclass.java
@@ -1,5 +1,11 @@
 public class Firstclass {
     public static void main(String[] args) {
-        System.out.println("hello ");
+        System.out.println("Sheetal");
+
+        System.out.println("Sheetal");
+
+        System.out.println("Sheetal");
+
     }
+
 }
