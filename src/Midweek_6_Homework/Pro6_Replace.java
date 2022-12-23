2dd95d13c760b4ada8117aaf8590de664a7236d9
Sheetu1807 <sdussooa@gmail.com>
2022-12-23 19:30:52 +0000

    Midweek Homework - Week 6 (Finished)

---
+++ src/Midweek_6_Homework/Pro6_Replace.java
@@ -0,0 +1,12 @@
+package Midweek_6_Homework;
+
+public class Pro6_Replace {
+
+    public static void main(String[] args) {
+
+        String name = " I@love@java"; //local variable
+
+        System.out.println(name.replaceAll("I@love@java", "we love java"));
+    }
+}
+
